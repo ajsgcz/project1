5b71a4bcbb77cf392b7503279a3819c734d1a99a
ajsgcz <160422427+ajsgcz@users.noreply.github.com>
2024-02-18 12:44:28 -0600

    Update oss.c

--- oss.c
+++ oss.c
@@ -1,81 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
-#include <getopt.h>
-
-#define MAX_CHILDREN 100
-
-void print_help(const char *progname) {
-    printf("Usage: %s [-h] [-n proc] [-s simul] [-t iter]\n", progname);
-    printf("Options:\n");
-    printf("  -h               Display this help message\n");
-    printf("  -n proc          Number of total children to launch\n");
-    printf("  -s simul         Maximum number of children running simultaneously\n");
-    printf("  -t iter          Number of iterations for each child process\n");
-}
 
 int main(int argc, char *argv[]) {
-    int num_children = 0;
-    int max_simultaneous = 0;
-    int iterations = 0;
+    int opt, proc = 0, simul = 0, iter = 0;
+    pid_t pid;
+    char iter_str[10];
 
-    int opt;
     while ((opt = getopt(argc, argv, "hn:s:t:")) != -1) {
         switch (opt) {
             case 'h':
-                print_help(argv[0]);
-                exit(EXIT_SUCCESS);
+                printf("Usage: oss [-h] [-n proc] [-s simul] [-t iter]\n");
+                return 0;
             case 'n':
-                num_children = atoi(optarg);
+                proc = atoi(optarg);
                 break;
             case 's':
-                max_simultaneous = atoi(optarg);
+                simul = atoi(optarg);
                 break;
             case 't':
-                iterations = atoi(optarg);
+                iter = atoi(optarg);
                 break;
             default:
-                print_help(argv[0]);
-                exit(EXIT_FAILURE);
+                printf("Invalid option\n");
+                return 1;
         }
     }
 
-    if (num_children <= 0 || max_simultaneous <= 0 || iterations <= 0) {
-        fprintf(stderr, "Error: Invalid arguments\n");
-        print_help(argv[0]);
-        exit(EXIT_FAILURE);
-    }
+    sprintf(iter_str, "%d", iter);
 
-    if (num_children > MAX_CHILDREN) {
-        fprintf(stderr, "Error: Maximum number of children exceeded\n");
-        exit(EXIT_FAILURE);
-    }
+    for (int i = 0; i < proc; i++) {
+        if ((pid = fork()) == 0) {
+            execl("./user", "./user", iter_str, NULL);
+        }
 
-    int children_running = 0;
-    for (int i = 0; i < num_children; i++) {
-        if (children_running >= max_simultaneous) {
-            // Wait for a child to terminate before forking a new one
+        if (i % simul == 0 && i != 0) {
             wait(NULL);
-            children_running--;
-        }
-
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            char iter_str[10];
-            snprintf(iter_str, sizeof(iter_str), "%d", iterations);
-            execl("./user", "user", iter_str, NULL);
-            perror("execl");
-            exit(EXIT_FAILURE);
-        } else {
-            children_running++;
         }
     }
 
-    // Wait for all remaining children to terminate
     while (wait(NULL) > 0);
 
     return 0;
